37e9c515f11f61493bc9b0c4844fafaa505274fc
rey sebastien <reyman64@gmail.com>
2014-04-06 20:44:38 +0200

    premier week end avril

---
+++ lnlink.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm ./Library/library.bib
+ln -s /home/reyman/Documents/bib/A_INPHD.bib ./Library/library.bib
+
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ln -s /home/srey/Copy/these/bib/A_INPHD.bib ./Library/library.bib
 xelatex These.tex
 biber These
 xelatex These.tex
