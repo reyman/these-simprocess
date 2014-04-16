239239011bf75aad140ed56e9b7066e2ffe10a6c
Sebastien Rey <reyman64@gmail.com>
2014-04-16 11:19:33 +0200

    fin positivisme, debut systemique

--- run_pandoc.sh
+++ run_pandoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pandoc Partie1.tex -o Partie1.odt --bibliography=library.bib --csl=/home/srey/Copy/these/manuscrit/chicago-author-date.csl
-pandoc Partie0.tex -o Partie0.odt --bibliography=library.bib --csl=/home/srey/Copy/these/manuscrit/chicago-author-date.csl
+pandoc Partie1.tex -o Partie1.odt --bibliography=Library/library.bib --csl=chicago-author-date.csl
+pandoc Partie0.tex -o Partie0.odt --bibliography=Library/library.bib --csl=chicago-author-date.csl
 
 
