8d8023bd4d44e1b6600b38fa2cb7a1bffbbe7194
Sebastien Rey <reyman64@gmail.com>
2014-04-23 18:51:34 +0200

    italics for denise

--- run_pandoc.sh
+++ run_pandoc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pandoc Partie1.tex -o Partie1.odt --bibliography=Library/library.bib --csl=chicago-author-date.csl
 pandoc Partie0.tex -o Partie0.odt --bibliography=Library/library.bib --csl=chicago-author-date.csl
+pandoc Partie0_italics.tex -o Partie0_italics.odt --bibliography=Library/library.bib --csl=chicago-author-date.csl
 
 
