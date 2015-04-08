125cf8f6a59979b746f94133da461055adda9182
Sebastien Rey <reyman64@gmail.com>
2015-04-08 09:02:48 +0200

    ajout schema wf et modification template

--- scripts/convert_svg.scala
+++ scripts/convert_svg.scala
@@ -18,7 +18,12 @@ import scala.sys.process._
 //val wd = processWorkingDir
 
 val wd = root/'home/'srey/'TRAVAUX/'THESE/'REPOSITORY_GIT/"simprocess-these"/'Schema_originaux
+val destination = root/'home/'srey/'TRAVAUX/'THESE/'REPOSITORY_GIT/"simprocess-these"/'FigurePartie2
 
 val results = (ls! wd |? (_.ext == "svg"))
 
 results.map{ r => Seq("sh","-c", "inkscape -z -D --file="+r+" --export-pdf="+r.toString.dropRight(4)+".pdf").!!}
+
+//val pdf = (ls! wd |? (_.ext == "pdf")) | cp.all
+
+
